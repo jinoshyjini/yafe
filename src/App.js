68de761a5656097e4eebcb2a26bca83be14e0fb1
jinoshyjini <91109544+jinoshyjini@users.noreply.github.com>
2024-10-15 13:53:18 +0400

    Re-added files to apply line ending settings

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{alignItems: 'center', padding: '150px', fontSize: '30px'}}>
+     Welcome to YAFE!
+    </div>
+  );
+}
+
+export default App;
